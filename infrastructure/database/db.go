ed00121001da58c7e86baa23593faaad944596c2
Varshard <varshard0@gmail.com>
2024-01-01 22:50:15 +0700

    fix slow user login

--- infrastructure/database/db.go
+++ infrastructure/database/db.go
@@ -4,10 +4,13 @@ import (
 	"github.com/varshard/mtl/infrastructure/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func InitDB(conf *config.DBConfig) (*gorm.DB, error) {
-	db, err := gorm.Open(mysql.Open(conf.ConnString))
+	db, err := gorm.Open(mysql.Open(conf.ConnString), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		return db, err
 	}
--- infrastructure/database/repository/user.go
+++ infrastructure/database/repository/user.go
@@ -14,7 +14,7 @@ type UserRepository struct {
 func (r UserRepository) FindUser(name string) (*database.User, error) {
 	u := &database.User{}
 	err := r.DB.Table(database.TableUser).Select("id, name, password").
-		Where("name = ?", name).Limit(1).First(&u).Error
+		Where("name = ?", name).Limit(1).Take(&u).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, xErr.NewErrNotFound(errors.New("user not found"))
 	} else if err != nil {
