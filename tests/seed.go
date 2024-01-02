ceae5e6c9ec750374219c40e877cdb9543f0af26
Varshard <varshard0@gmail.com>
2024-01-02 21:42:51 +0700

    update test seed script

--- tests/seed.go
+++ tests/seed.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 func SeedDB(db *gorm.DB) error {
 	return db.Exec(`
-INSERT INTO vote_item VALUES (1,'item1','description',1),(2,'item 2','Created by John',2),(3,'voted item','it has some votes',1);
+INSERT INTO vote_item VALUES (1,'item1','description'),(2,'item 2','Created by John'),(3,'voted item','it has some votes');
 `).Error
 }
 
